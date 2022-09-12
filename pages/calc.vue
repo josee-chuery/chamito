cb77f12d344ed37806703c4be454c4f92201760e
jose-elias <josesoliveira2009@hotmail.com>
2022-09-12 11:13:11 -0300

    calculadora

---
+++ pages/calc.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1 class="title">
+            {{mensagem}}
+        </h1>
+        <br><br>
+        <b-field label="Número 1">
+            <b-input v-model="numero1"></b-input>
+        </b-field>
+        <b-field label="Número 2">
+            <b-input v-model="numero2"></b-input>
+        </b-field>
+        <b-button type="is-info" @click="soma()">
+            +
+        </b-button>
+        <b-button type="is-info" @click="menos()">
+            -
+        </b-button>
+        <b-button type="is-info" @click="multi()">
+            *
+        </b-button>
+        <b-button type="is-info" @click="divisao()">
+            /
+        </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mensagem: "R$ 0,00",
+            numero1: 0,
+            numero2: 0,
+            resultado: 0
+        }
+    },
+    methods: {
+        teste (){
+            this.mensagem = "Novo número";
+        },
+        soma (){
+            this.mensagem = parseInt(this.numero1) + parseInt(this.numero2);
+        },
+        menos (){
+            this.mensagem = parseInt(this.numero1) - parseInt(this.numero2);
+        },
+        multi (){
+            this.mensagem = parseInt(this.numero1) * parseInt(this.numero2);
+        },
+        divisao (){
+            this.mensagem = parseInt(this.numero1) / parseInt(this.numero2)
+        }
+    }
+}
+</script>
--- pages/carrinho.vue
+++ pages/carrinho.vue
@@ -6,47 +6,58 @@
         <thead>
           <tr>
             <th>Produto</th>
-            <th>Valor Unitário</th>
             <th>Quantidade</th>
+            <th>Valor Unitário</th>
             <th>Valor Total</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>Produto 1</th>
-            <td>R$ 2,00</td>
+          <tr v-for="produto in produtos" :key="produto.nome">
+            <td>{{produto.nome}}</td>
             <td>
               <b-field>
-                <b-numberinput controls-position="compact" v-model="number" type="is-info"></b-numberinput>
+                <b-numberinput controls-position="compact" v-model="quantidade1" type="is-info"></b-numberinput>
               </b-field>
             </td>
-            <td>R$ 2,00</td>
-          </tr>
-          <tr>
-            <th>Produto 2</th>
-            <td>R$ 3,00</td>
-            <td>
-              <b-field>
-                <b-numberinput controls-position="compact" v-model="number" type="is-info"></b-numberinput>
-              </b-field>
-            </td>
-            <td>R$ 3,00</td>
-          </tr>
-          <tr>
-            <th>Produto 3</th>
-            <td>R$ 4,00</td>
-            <td>
-              <b-field>
-                <b-numberinput controls-position="compact" v-model="number" type="is-info"></b-numberinput>
-              </b-field>
-            </td>
-            <td>R$ 3,00</td>
+            <td>{{produto.nome}}</td>
           </tr>
         </tbody>
       </table>
     </div>
-    <h1 class="title">Preço Total: R$ 6,00</h1>
+    <h1 class="title">Preço Total: R$ {{resultado}}</h1>
     <b-button type="is-info">Finalizar Compra</b-button>
     <b-button type="is-info">Contunuar Comprando</b-button>
   </section>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            mensagem: "0.00",
+
+            produtos: [
+              {
+                nome: "produto1",
+                valor: 2.00,
+                quantidade: 1
+              },
+              {
+                nome: "produto2",
+                valor: 3.00,
+                quantidade: 1
+              },
+              {
+                nome: "produto3",
+                valor: 4.00,
+                quantidade: 1
+              }
+            ]
+        }
+    },
+    methods: {
+        teste (){
+            this.mensagem = "Novo número";
+        }
+    }
+}
+</script>
